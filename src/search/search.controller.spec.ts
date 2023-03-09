b2ab7bf5f84c882073a3b5bf916114d8dc5790ac
cloudewu <boxbox557@gmail.com>
2023-03-09 22:31:55 +0800

    refactor: search controller spec

--- src/search/search.controller.spec.ts
+++ src/search/search.controller.spec.ts
@@ -1,37 +1,36 @@
+import { createMock, type DeepMocked } from '@golevelup/ts-jest';
 import { Test, type TestingModule } from '@nestjs/testing';
 import { FilterType } from '../dto/filter-type.enum';
-import { PlurksDto } from '../dto/plurks.dto';
+import { SearchResponseDto } from '../dto/searchResponse.dto';
 import { SearchController } from './search.controller';
 import { SearchService } from './search.service';
 
 describe('SearchController', () => {
-  const fakeToken = 'Fake token';
-
   let app: TestingModule;
 
   beforeAll(async() => {
     app = await Test.createTestingModule({
       controllers: [SearchController],
-    }).useMocker(token => {
-      if (token === SearchService) {
-        const response = new PlurksDto();
-        return { search: jest.fn().mockResolvedValue(response) };
-      }
-    }).compile();
+    }).useMocker(createMock)
+      .compile();
+
+    const service: DeepMocked<SearchService> = app.get(SearchService);
+    service.search.mockResolvedValue(new SearchResponseDto());
   });
 
   describe('getSearch', () => {
     it('should accept available parameters', async() => {
       // given
+      const token = 'This is a token';
       const query = 'Search query';
       const filter = FilterType.MY;
       const offset = new Date('2023-03-04T00:00:00.000Z').toISOString();
       // when
       const controller = app.get(SearchController);
-      await controller.getSearch(fakeToken, query, filter, offset);
+      await controller.getSearch(token, query, filter, offset);
       // then
       const service = app.get(SearchService);
-      expect(service.search).toHaveBeenCalledWith(fakeToken, query, filter, offset);
+      expect(service.search).toHaveBeenCalledWith(token, query, filter, offset);
     });
   });
 });
