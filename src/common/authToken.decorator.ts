b1efb33ae279f10f09074dc793e904a607800061
cloudewu <boxbox557@gmail.com>
2023-08-04 17:02:10 +0800

    fix: 500 when searching with empty authorization header
    fixs #4

--- src/common/authToken.decorator.ts
+++ src/common/authToken.decorator.ts
@@ -4,6 +4,6 @@ export const AuthToken = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const headers = ctx.switchToHttp().getRequest().headers;
     const token = headers.authorization;
-    return token.replace('Bearer ', '');
+    return token?.replace('Bearer ', '');
   },
 );
