9fd6b08bb0030e88aa1e00c900a1df081121a22e
cloudewu <boxbox557@gmail.com>
2023-08-15 22:47:38 +0800

    feat: redirect from /search if token is missing

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/search',
+        missing: [
+          {
+            type: 'cookie',
+            key: 'token'
+          }
+        ],
+        destination: '/auth',
+        permanent: false
+      }
+    ];
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
 
 module.exports = nextConfig;
