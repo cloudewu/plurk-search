9f1f1741965cf261cc6741a132b15a037a6321c6
cloudewu <boxbox557@gmail.com>
2023-09-05 00:07:55 +0800

    ui: improve alert message

--- web/app/auth/AlreadyLoggedInAlert.tsx
+++ web/app/auth/AlreadyLoggedInAlert.tsx
@@ -11,12 +11,13 @@ export default function AlreadyLoggedInAlert() {
           href='/search'
           variant='outlined'
           color='primary'
-          sx={{ mx: 0.5 }}
+          sx={{ my: 'auto', minWidth: 'max-content' }}
         >
           開始搜尋
         </NavButton>
-      }>
-      你已經申請過驗證碼囉，是否前往搜尋？
+      }
+    >
+      你已經申請過驗證碼囉！重新驗證將使原先的授權失效。
     </Alert>
   );
 };
