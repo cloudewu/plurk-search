27d56e65b450616e2b6a081986bbe5e03c3a947a
cloudewu <boxbox557@gmail.com>
2023-09-07 21:17:11 +0800

    (api) feat: log traffic in /auth

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,14 +1,17 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Logger, Post } from '@nestjs/common';
 import { AuthToken } from '../common/authToken.decorator';
 import type { AuthResponseDto } from '../dto/authResponse.dto';
 import { AuthService } from './auth.service';
 
 @Controller('/auth')
 export class AuthController {
+  private readonly logger = new Logger(AuthController.name);
+
   constructor(private readonly authService: AuthService) {}
 
   @Get()
   async getAuthenticationPage(): Promise<AuthResponseDto> {
+    this.logRequest('/auth', {});
     return await this.authService.getAuthenticationLink();
   }
 
@@ -17,6 +20,11 @@ export class AuthController {
   async postAuthenticate(
     @AuthToken() token: string,
       @Body('code') code: string): Promise<string> {
+    this.logRequest('/auth', { token, code });
     return await this.authService.authenticate(token, code);
+  }
+
+  private logRequest(endpoint: string, params: any) {
+    this.logger.log(`Received Request: ${endpoint}; params: ${JSON.stringify(params)}`);
   }
 }
